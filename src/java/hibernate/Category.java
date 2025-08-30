3f13601b2152873da29500399f3deb8251d324a5
xPamii <pbherath2018@gmail.com>
2025-08-30 23:58:42 +0530

    category entity

---
+++ src/java/hibernate/Category.java
@@ -0,0 +1,48 @@
+package hibernate;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pamii
+ */
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "value", length = 45, nullable = false)
+    private String value;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
+
+}
