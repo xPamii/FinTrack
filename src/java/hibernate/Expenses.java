6ade8964d06712b9e04059def52fa6d696a20c70
xPamii <pbherath2018@gmail.com>
2025-08-31 00:49:09 +0530

    expenses entity

---
+++ src/java/hibernate/Expenses.java
@@ -0,0 +1,73 @@
+package hibernate;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pamii
+ */
+@Entity
+@Table(name = "expenses")
+public class Expenses implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private User user;
+
+
+    @Column(name = "amount", nullable = false)
+    private double amount;
+
+    @Column(name = "created_at", nullable = false)
+    private Date created_at;
+
+    public Expenses() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+}
